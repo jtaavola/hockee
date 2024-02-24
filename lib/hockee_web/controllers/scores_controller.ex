876a9dc636e02d68646d42cd458622b80caa5992
jtaavola <jesse@taavola.dev>
2024-02-24 11:32:48 -0600

    improve game score cards

--- lib/hockee_web/controllers/scores_controller.ex
+++ lib/hockee_web/controllers/scores_controller.ex
@@ -2,6 +2,39 @@ defmodule HockeeWeb.ScoresController do
   use HockeeWeb, :controller
 
   def scores(conn, _params) do
-    render(conn, :scores)
+    games = [
+      %{
+        awayTeam: %{
+          name: "Wild",
+          goals: 4,
+          sog: 32,
+          logo: "https://assets.nhle.com/logos/nhl/svg/MIN_light.svg"
+        },
+        homeTeam: %{
+          name: "Jets",
+          goals: 1,
+          sog: 22,
+          logo: "https://assets.nhle.com/logos/nhl/svg/WPG_light.svg"
+        },
+        clock: "FINAL"
+      },
+      %{
+        awayTeam: %{
+          name: "Golden Knights",
+          goals: 0,
+          sog: 32,
+          logo: "https://assets.nhle.com/logos/nhl/svg/VGK_light.svg"
+        },
+        homeTeam: %{
+          name: "Blue Jackets",
+          goals: 1,
+          sog: 22,
+          logo: "https://assets.nhle.com/logos/nhl/svg/CBJ_light.svg"
+        },
+        clock: "FINAL"
+      }
+    ]
+
+    render(conn, :scores, games: games)
   end
 end
